942f93947521ff4851a0c1d543a8fc758b99b4de
Jose Luis Ferrete <joseluisferrete@gmail.com>
2018-12-23 15:00:07 +0100

    Leccion 7: Scroll infinito con Laravel y VUEjs

--- resources/js/components/PostsComponent.vue
+++ resources/js/components/PostsComponent.vue
@@ -3,12 +3,11 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
 
-
-
                 <div class="card mb-3 mt-3" v-for="item in list">
                     <div class="card-header" v-bind:href="item.slug" v-text="item.title"></div>
 
                     <div class="card-body">
+                        <p>ID: {{ item.id }}</p>
                         <p class="card-text" v-text="item.body"></p>
                     </div>
                 </div>
@@ -29,7 +28,21 @@
         },
         methods: {
             infiniteHandler($state){
-                //conectar al servidor
+                this.page++
+                let url = 'http://vuejslaravel.local/api/posts?page=' + this.page
+                //http://vuejslaravel.local/api/posts?page=4
+
+                axios.get(url)
+                    .then(response => {
+                        let posts = response.data.data
+
+                        if(posts.length) {
+                            this.list = this.list.concat(posts)
+                            $state.loaded()
+                        }else{
+                            $state.complete()
+                        }
+                    });
             }
         }
     }
