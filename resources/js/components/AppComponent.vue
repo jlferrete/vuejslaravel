edabcf59d1bbdf43c2b1b0aca91a3d28670c8a4b
Jose Luis Ferrete <joseluisferrete@gmail.com>
2019-01-30 19:01:22 +0100

    Leccion 11: Introducción a plantillas Bootstrap 4 dentro de VUEjs

--- resources/js/components/AppComponent.vue
+++ resources/js/components/AppComponent.vue
@@ -1,1 +1,42 @@
-AppComponent.vue
+<template>
+    <div>
+        <header>
+            <div class="collapse bg-dark" id="navbarHeader">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-8 col-md-7 py-4">
+                            <h4 class="text-white">SPA y componentes</h4>
+                            <p class="text-muted">Curso básico para ayudar a entender bien esta tecnología.</p>
+                        </div>
+                        <div class="col-sm-4 offset-md-1 py-4">
+                            <h4 class="text-white">Navegación</h4>
+                            <ul class="list-unstyled">
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="navbar navbar-dark bg-dark shadow-sm">
+                <div class="container d-flex justify-content-between">
+                    <a href="#" class="navbar-brand d-flex align-items-center">
+                        <strong>Rimorsoft online</strong>
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+            </div>
+        </header>
+
+        <section class="jumbotron text-center">
+            <div class="container">
+                <h1 class="jumbotron-heading">Album example</h1>
+                <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+                <p>
+
+                </p>
+            </div>
+        </section>
+    </div>
+</template>
